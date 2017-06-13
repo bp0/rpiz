5440f08d84dba4ef5fa391c71db2d50ddeba2c99
Burt P <pburt0@gmail.com>
2017-06-12 21:18:15 -0500

    Board info tweaks for Raspberry Pi
    Signed-off-by: Burt P <pburt0@gmail.com>

--- src/board_rpi.c
+++ src/board_rpi.c
@@ -52,9 +52,9 @@ static struct {
   { "0013",      "Q1 2015",    "B+",                  "1.2",    "512MB",    unk,             NULL },
   { "0014",      "Q2 2014",    "Compute Module 1",    "1.0",    "512MB",    "Embest",        NULL },
   { "0015",      unk,          "A+",                  "1.1",    "256MB/512MB",    "Embest",      NULL  },
-  { "a01040",    unk,          "2 Model B",           "1.0",    "1GB",      "Sony",          "BCM2836?" },
-  { "a01041",    "Q1 2015",    "2 Model B",           "1.1",    "1GB",      "Sony",          "BCM2836?" },
-  { "a21041",    "Q1 2015",    "2 Model B",           "1.1",    "1GB",      "Embest",        "BCM2836?" },
+  { "a01040",    unk,          "2 Model B",           "1.0",    "1GB",      "Sony",          "BCM2836" },
+  { "a01041",    "Q1 2015",    "2 Model B",           "1.1",    "1GB",      "Sony",          "BCM2836" },
+  { "a21041",    "Q1 2015",    "2 Model B",           "1.1",    "1GB",      "Embest",        "BCM2836" },
   { "a22042",    "Q3 2016",    "2 Model B",           "1.2",    "1GB",      "Embest",        "BCM2837" },  /* (with BCM2837) */
   { "900021",    "Q3 2016",    "A+",                  "1.1",    "512MB",    "Sony",          NULL },
   { "900032",    "Q2 2016?",    "B+",                 "1.2",    "512MB",    "Sony",          NULL },
@@ -64,7 +64,7 @@ static struct {
   { "9000c1",    "Q1 2017",    "Zero W",              "1.1",    "512MB",    "Sony",          NULL },
   { "a02082",    "Q1 2016",    "3 Model B",           "1.2",    "1GB",      "Sony",          "BCM2837" },
   { "a020a0",    "Q1 2017",    "Compute Module 3 or CM3 Lite",  "1.0",    "1GB",    "Sony",          NULL },
-  { "a22082",    "Q1 2016",    "3 Model B",           "1.2",    "1GB",      "Embest",        "BCM2709" },
+  { "a22082",    "Q1 2016",    "3 Model B",           "1.2",    "1GB",      "Embest",        "BCM2837" },
   { "a32082",    "Q4 2016",    "3 Model B",           "1.2",    "1GB",      "Sony Japan",    NULL  },
   { NULL, NULL, NULL, NULL, NULL, NULL, NULL }
 };
@@ -77,7 +77,7 @@ struct rpi_board {
     /* all from /proc/cpuinfo */
     char *soc, *revision, *serial;
 
-    int overvolt; /* revision starts with 1000 */
+    int overvolt; /* revision starts with 1000 (or maybe just 1) */
 
     /* all point into the rpi_boardinfo table -
      * no need to free */
@@ -97,16 +97,36 @@ int rpi_board_check() {
     return ret;
 }
 
+/* return number of chars to skip */
+static int ov_check(const char *r_code) {
+    /* sources differ. prefix is either 1000... or just 1... */
+    //if (strncmp(r, "1000", 4) == 0)
+    //    return 4;
+    if (strncmp(r_code, "1", 1) == 0)
+        return 1;
+    return 0;
+}
+
+static int code_match(const char* code0, const char* code1) {
+    int c0, c1;
+    if (code0 == NULL || code1 == NULL) return 0;
+    c0 = strtol(code0, NULL, 16);
+    c1 = strtol(code1, NULL, 16);
+    if (c0 && c1)
+        return (c0 == c1) ? 1 : 0;
+    else
+        return (strcmp(code0, code1) == 0) ? 1 : 0;
+}
+
 static int rpi_find_board(const char *r_code) {
     int i = 0;
     char *r = (char*)r_code;
     if (r_code == NULL)
         return 0;
     /* ignore the overvolt prefix */
-    if (strncmp(r, "1000", 4) == 0)
-        r += 4;
+    r += ov_check(r_code);
     while (rpi_boardinfo[i].value != NULL) {
-        if (strcmp(r, rpi_boardinfo[i].value) == 0)
+        if (code_match(r, rpi_boardinfo[i].value))
             return i;
 
         i++;
@@ -174,7 +194,7 @@ rpi_board *rpi_board_new() {
         s->soc_spec = rpi_boardinfo[i].soc;
         s->overvolt = 0;
         if (s->revision)
-            if (strncmp(s->revision, "1000", 4) == 0)
+            if (ov_check(s->revision))
                 s->overvolt = 1;
 
         s->dt_model = get_dt_string("model");
@@ -304,6 +324,7 @@ static char* rpi_board_overvolt_str(rpi_board *s) {
 }
 
 #define ADDFIELD(t, l, o, n, f) fields_update_bytag(s->fields, t, l, o, n, (rpiz_fields_get_func)f, (void*)s)
+#define ADDFIELDSTR(t, l, o, n, str) fields_update_bytag(s->fields, t, l, o, n, NULL, (void*)str)
 rpiz_fields *rpi_board_fields(rpi_board *s) {
     if (s) {
         if (!s->fields) {
@@ -315,7 +336,9 @@ rpiz_fields *rpi_board_fields(rpi_board *s) {
             ADDFIELD("board.rpi_intro",     0, 0, "Introduction", rpi_board_intro );
             ADDFIELD("board.rpi_mfgby",     0, 0, "Manufacturer", rpi_board_mfgby );
             ADDFIELD("board.rpi_mem_spec",  0, 0, "Memory (spec)", rpi_board_mem_spec );
-            ADDFIELD("board.rpi_soc_spec",  0, 0, "SOC (spec)", rpi_board_soc );
+            if (s->soc_spec)
+            ADDFIELDSTR("board.rpi_soc_spec",  0, 0, "SOC (spec)", s->soc_spec );
+            ADDFIELDSTR("board.rpi_soc",    0, 0, "SOC (reported)", s->soc );
             ADDFIELD("board.rpi_rcode",     0, 0, "RCode", rpi_board_rcode );
             ADDFIELD("board.rpi_serial",    0, 0, "Serial Number", rpi_board_serial );
             ADDFIELD("board.rpi_overvolt",  0, 1, "Overvolt", rpi_board_overvolt_str );
